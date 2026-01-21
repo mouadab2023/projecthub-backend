a361515b903dc6fb61bfd47ff41d7788cdfca968
mouadab2023 <144780358+mouadab2023@users.noreply.github.com>
2026-01-21 21:59:04 +0100

    add task entity

---
+++ src/main/java/org/example/projecthubbackend/entities/Task.java
@@ -0,0 +1,53 @@
+package org.example.projecthubbackend.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.projecthubbackend.enumerations.Priority;
+import org.example.projecthubbackend.enumerations.Status;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String description;
+
+    @NotNull
+    private LocalDate dueDate;
+
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    private Status status=Status.TODO;
+
+    @CreationTimestamp
+    private LocalDateTime createTime;
+
+    @UpdateTimestamp
+    private LocalDateTime updateTime;
+
+
+}
+
