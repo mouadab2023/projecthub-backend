fd97af867dd352ed6949c743f93bc3766bf90cef
mouadab2023 <144780358+mouadab2023@users.noreply.github.com>
2026-01-21 21:37:49 +0100

    project init

---
+++ src/main/java/org/example/projecthubbackend/ProjecthubBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.projecthubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecthubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjecthubBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projecthubbackend/ProjecthubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projecthubbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecthubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
