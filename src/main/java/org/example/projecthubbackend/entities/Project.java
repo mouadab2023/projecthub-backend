bc72a8a3911ef7b371283d741ff56295561e3511
mouadab2023 <144780358+mouadab2023@users.noreply.github.com>
2026-01-21 22:00:18 +0100

    add project entity

---
+++ src/main/java/org/example/projecthubbackend/entities/Project.java
@@ -0,0 +1,51 @@
+package org.example.projecthubbackend.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotNull
+    private LocalDate startDate;
+
+    @NotNull
+    private LocalDate endDate;
+
+    @CreationTimestamp
+    private LocalDateTime createTime;
+
+    @UpdateTimestamp
+    private LocalDateTime updateTime;
+
+    @AssertTrue(message = "startDate doit être avant endDate")
+    public boolean isStartDateBeforeEndDate() {
+        if (startDate == null || endDate == null) return true; // @NotNull gère les nulls
+        return startDate.isBefore(endDate);
+    }
+
+}
