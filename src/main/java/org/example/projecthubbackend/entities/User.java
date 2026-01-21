24c8f0ec0e27d789ae3cf646c96ada07214d9a5b
mouadab2023 <144780358+mouadab2023@users.noreply.github.com>
2026-01-21 22:12:36 +0100

    add user entity

---
+++ src/main/java/org/example/projecthubbackend/entities/User.java
@@ -0,0 +1,54 @@
+package org.example.projecthubbackend.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "app_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    private String avatarUrl;
+
+    @NotNull
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles;
+
+    @CreationTimestamp
+    private LocalDateTime createTime;
+
+    @UpdateTimestamp
+    private LocalDateTime updateTime;
+
+}
+
