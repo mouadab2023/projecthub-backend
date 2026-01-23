7c2e133ebabc6522d3a9e32d647f18212112f3b8
mouadab2023 <144780358+mouadab2023@users.noreply.github.com>
2026-01-23 12:22:08 +0100

    add task dto

---
+++ src/main/java/org/example/projecthubbackend/dtos/TaskDto.java
@@ -0,0 +1,38 @@
+package org.example.projecthubbackend.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+import org.example.projecthubbackend.enumerations.Priority;
+import org.example.projecthubbackend.enumerations.Status;
+import org.hibernate.sql.Update;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class TaskDto {
+    @NotNull(groups = Update.class)
+    Long id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String description;
+
+    @NotNull
+    private LocalDate dueDate;
+
+    @NotNull
+    private Priority priority;
+
+    @Builder.Default
+    private Status status=Status.TODO;
+
+    @NotNull
+    private Long project;
+
+    private Long assignee;
+}
