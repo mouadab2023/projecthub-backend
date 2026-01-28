1afc2309854c46fa70a3c4e5845bccb3e7a64909
mouadab2023 <144780358+mouadab2023@users.noreply.github.com>
2026-01-28 22:30:49 +0100

    config dockerfile docker-compose  and env file

--- src/main/java/org/example/projecthubbackend/configs/ApplicationConfiguration.java
+++ src/main/java/org/example/projecthubbackend/configs/ApplicationConfiguration.java
@@ -3,6 +3,9 @@ package org.example.projecthubbackend.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -13,7 +16,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ApplicationConfiguration {
-
+    @Profile("!docker")
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        configurer.setLocation(new FileSystemResource(".env"));
+        return configurer;
+    }
     @Bean
     BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
